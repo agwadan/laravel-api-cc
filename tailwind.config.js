6003e7e45c115ca32e64bced34ece7d53fa1e2fd
agwadan <agwa.dan@gmail.com>
2023-11-20 12:05:57 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*{html,js}'], //--------watch for html and js files in the src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
